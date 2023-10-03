ce86062b5c948112e49a7f53bf71e666c1417d36
tanradap <tanradap@gmail.com>
2023-10-03 12:13:09 +0700

    delete files

--- Qupath_scripts/Set_detection_name.groovy
+++
@@ -1,8 +0,0 @@
-selectedAnnotations = getAnnotationObjects().findAll{it.getPathClass() == getPathClass("GP")}
-selectedAnnotations.each{anno->
-    selectedCells = getCurrentHierarchy().getObjectsForROI(qupath.lib.objects.PathDetectionObject, anno.getROI())
-}
-selectedCells.each{cell->
-    cell.setName("Globus Pallidus")
-
-}
--- Qupath_scripts/objects_within_annotations.groovy
+++
@@ -1,8 +0,0 @@
-selectedAnnotations = getAnnotationObjects().findAll{it.getPathClass() == getPathClass("Ignore")}
-selectedAnnotations.each{anno->
-    selectedCells = getCurrentHierarchy().getObjectsForROI(qupath.lib.objects.PathDetectionObject, anno.getROI())
-}
-selectedCells.each{cell->
-    getCurrentHierarchy().getSelectionModel().setSelectedObject(cell, true);
-
-}
